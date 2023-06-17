689dd3d1d39baa0814757b1f60b70dff93d72cfa
Filipe Lopes <filipe.lopes@solides.com.br>
2023-06-17 15:10:32 -0300

    lista de petz da pagina de comparacao rodando dinamicamente conforme arquivo de json petz.txt

--- src/pages/MeusPetz/comparacao/comparacao.js
+++ src/pages/MeusPetz/comparacao/comparacao.js
@@ -53,16 +53,107 @@ elem.innerHTML = newHtml;
 
 
 //Lista todos os petz desaparecidos précadastrados( esta lista não sofre interferência do usuário)
+var listaAnimaisGenericos = null;
 fetch('../../../data/petz.txt')
   .then(response => response.text())
   .then(text => {
 
-  var listaAnimaisGenericos = null;
-  console.log(listaAnimaisGenericos = JSON.parse(text));
+	  listaAnimaisGenericos = JSON.parse(text).petz;
+	  console.log(listaAnimaisGenericos);
+	  console.log(listaAnimaisGenericos[0]);
+
+		var elemList = document.querySelector('#colunaAnimalListaGenerico');
+	  newHtml = ' ';//row
+
+	  //estrutura inicial da section
+
+    newHtml += '            <!-- Subtítulo -->';
+    newHtml += '            <div class="my-3 mb-4 col-sm-8 mx-auto text-center">';
+    newHtml += '                <h2 class="text-secondary">Achamos que esses animais são semelhantes ao seu, por favor verifique</h2>';
+    newHtml += '            </div>';
+
+    newHtml += '            <div class="row mb-5 mt-0">';
+
+		for (var i = 0; i <= Object.keys(listaAnimaisGenericos).length - 1; i++) {
+			// console.log((i+1) / 3);
+			// console.log(Number.isInteger((i+1) / 3));
+			if (i > 5) { newHtml += '</div>';break;}
+		  var eachPetlistItem = listaAnimaisGenericos[i];
+			// console.log(eachPetlistItem);
+			newHtml += '							<div class="col-4 ' + (Number.isInteger((i+1) / 3) === false ? "border-end" : "") + '">';
+      newHtml += '                  <div class="row py-4">';
+      newHtml += '                      <!-- Foto(s) -->';
+      newHtml += '                      <div class="col-sm-10 mx-auto">';
+      newHtml += '                          <div class="mx-auto w-100" style="max-height: 400px;">';
+      newHtml += '                              <img src="' + ( eachPetlistItem.imgPath ? eachPetlistItem.imgPath : "https://placehold.co/500" ) + 'https://placehold.co/500" class="img-thumbnail rounded d-block mx-auto" alt="...">';
+      newHtml += '                          </div>';
+      newHtml += '                      </div>';
+      newHtml += '                  </div>';
+      newHtml += '                  <div class="row p-0">';
+      newHtml += '                      <div class="w-100 text-center">';
+      newHtml += '                          <h2>Pet 1</h2>';
+      newHtml += '                      </div>';
+      newHtml += '                  </div>';
+      newHtml += '                  <div class="row py-4">';
+      newHtml += '                      <div class="col-sm-10 mx-auto">';
+      newHtml += '                      <!-- Tag / Características -->';
+      newHtml += '                          <!-- <ul class="list-group list-group-flush"> -->';
+      newHtml += '                              <button type="button" class="btn btn-info mx-1 my-1">' + eachPetlistItem.especie + '</button>';
+      newHtml += '                              <button type="button" class="btn btn-info mx-1 my-1">' + eachPetlistItem.raca + '</button>';
+      newHtml += '                              <button type="button" class="btn btn-info mx-1 my-1">' + eachPetlistItem.porte + '</button>';
+      newHtml += '                              <button type="button" class="btn btn-info mx-1 my-1">' + eachPetlistItem.idade + '</button>';
+      newHtml += '                              <button type="button" class="btn btn-info mx-1 my-1">' + eachPetlistItem.olhos_cor + '</button>';
+      newHtml += '                              <button type="button" class="btn btn-info mx-1 my-1">' + (eachPetlistItem.chipado == "Não" ? "Não " :"" ) + 'Possui chip</button>';
+      newHtml += '                              <button type="button" class="btn btn-info mx-1 my-1">' + (eachPetlistItem.castrado == "Não" ? "Não é " :"" ) + 'Castrado</button>';
+      newHtml += '                              <button type="button" class="btn btn-info mx-1 my-1">' + eachPetlistItem.docil + '</button>';
+      newHtml += '                              <button type="button" class="btn btn-info mx-1 my-1">' + eachPetlistItem.genero + '</button>';
+      newHtml += '                          <!-- </ul> -->';
+      newHtml += '                      </div>';
+      newHtml += '                  </div>';
+      newHtml += '              </div>';
 
 
-  for (var i = listaAnimaisGenericos.length - 1; i >= 0; i--) {
-  	console.log(listaAnimaisGenericos[i]);
-  }
+      console.log((i+1));
+      console.log((i+1) / 3);
+      console.log(Number.isInteger((i+1) / 3));
+			if ( Number.isInteger((i+1) / 3) == true ){
+				newHtml += '</div><div class="row mb-5 mt-0">'
+			}
+		}
 
-});
+	  newHtml += ' ';//row
+
+	  newHtml += '						<div class="row ">';
+    newHtml += '                <div class="col-6 mx-auto">';
+    newHtml += '                    <nav aria-label="...">';
+    newHtml += '                        <ul class="pagination">';
+    newHtml += '                          <li class="page-item disabled">';
+    newHtml += '                            <span class="page-link">Anterior</span>';
+    newHtml += '                          </li>';
+    newHtml += '                          <li class="page-item active"  aria-current="page">';
+    newHtml += '                            <span class="page-link">1</span>';
+    newHtml += '                          </li>';
+    newHtml += '                          <li class="page-item">';
+    newHtml += '                            <a class="page-link" href="#">2</a>';
+    newHtml += '                          </li>';
+    newHtml += '                          <li class="page-item"><a class="page-link" href="#">3</a></li>';
+    newHtml += '                          <li class="page-item"><a class="page-link" href="#">4</a></li>';
+    newHtml += '                          <li class="page-item disabled" aria-current="page">';
+    newHtml += '                            <span class="page-link">...</span>';
+    newHtml += '                          </li>';
+    newHtml += '                          <li class="page-item"><a class="page-link" href="#">19</a></li>';
+    newHtml += '                          <li class="page-item"><a class="page-link" href="#">20</a></li>';
+    newHtml += '                          <li class="page-item"><a class="page-link" href="#">21</a></li>';
+    newHtml += '                          <li class="page-item">';
+    newHtml += '                            <a class="page-link" href="#">Próxima</a>';
+    newHtml += '                          </li>';
+    newHtml += '                        </ul>';
+    newHtml += '                    </nav>';
+    newHtml += '                </div>';
+    newHtml += '            </div>';
+
+	  // console.log(newHtml);
+		
+		elemList.innerHTML = newHtml;
+
+	});
